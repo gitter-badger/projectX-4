3dd4ce4ae7d3a128cf8ed0422361a719d4c7f9fa
Elsabaa <abdelrahman.elsabaa@gmail.com>
2016-07-17 14:35:05 +0200

    handelling some errors and ignoring the .lock

--- NadaStore/app/controllers/user_controller.rb
+++ NadaStore/app/controllers/user_controller.rb
@@ -1,8 +1,11 @@
 class UserController < ApplicationController 
   def create 
-  	@payload = '{"user":'+'{"username":"' + params[:user][:username].to_s + '", "password":"' + params[:user][:password].to_s + '"' +
-  	+ ', "firstname":"' + params[:user][:firstname].to_s + '"' ++ ', "email":"' + params[:user][:email].to_s + '"' + '}}'
-	@res_json = RestClient.post 'http://localhost:3030/users', @payload, :content_type => 'application/json'
+  payload = {:username => params[:user][:username].to_s, :password => params[:user][:password].to_s,
+              :firstname => params[:user][:firstname].to_s, :email => params[:user][:email].to_s,
+              :lastname => params[:user][:lastname].to_s,
+            }
+  payload_json = payload.to_json   
+	@res_json = RestClient.post 'http://localhost:3030/users', payload_json, :content_type => 'application/json'
 	@res = JSON.parse(@res_json)
 	@done = "user has been created successfully!!"
 	# redirect_to welcome_signup_path
