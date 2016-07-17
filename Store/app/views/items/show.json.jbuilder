ff64ff4e942309481648633d3b0acc9188bea4ff
Ahmed Kamel <ahm.kam92@gmail.com>
2016-07-18 00:19:24 +0200

    Added basic MVC for the Store Items

---
+++ Store/app/views/items/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @item, :id, :title, :price, :quantity, :created_at, :updated_at
---
+++ Store/db/migrate/20160717164134_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string :title
+      t.float :price
+      t.integer :quantity
+
+      t.timestamps null: false
+    end
+  end
+end
